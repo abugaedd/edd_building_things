847b618129ba174ebdf780e7e50e5536f7b1e854
abugaedd <abugaeddie@gmail.com>
2022-12-01 04:53:39 -0800

    more math

---
+++ moremath.py
@@ -0,0 +1,10 @@
+# Convert miles to kilometres
+# km = miles * 1.60934
+# Enter Miles
+# 5 miles = 8.04 km
+
+miles = input("Enter Miles: ")
+miles = int(miles)
+km = miles * 1.60934
+print("{} * {} = {}".format(miles, 1.60934, km))
+print('kilometers is ', km)
